a19be47c1352c495acd0f2e73699f501f87dbe73
Juliansyah Rizky <jratrainbows@gmail.com>
2023-06-21 10:36:45 +0700

    test to Deploy at Netlify

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ function Header() {
                     <li><a href="#about">About Me</a></li>
                     <li><a href="#HIRE">HIRE ME!</a></li>
                     <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#contact">Contact Me</a></li>
+                    <li><a href="#contact">Contact Me!</a></li>
                 </ul>
         </div>
         <div className="bx bx-menu" id="menu-icon"></div>
